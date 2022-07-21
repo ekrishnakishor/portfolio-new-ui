67880feae109853d31d8facfca738785e6ad7246
ekrishnakishor <ekrishnakishor@gmail.com>
2022-07-21 15:13:17 +0530

    added js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function hamBurger() {
+    var x = document.getElementById("mmenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
